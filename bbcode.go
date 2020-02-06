0156804a359db9d949ee2c80bc7e00fdfcf4abc3
andrew <me@prgr.org>
2020-02-06 13:00:58 +0300

    init

---
+++ bbcode.go
@@ -0,0 +1,167 @@
+package bbcode
+
+import (
+	"strings"
+)
+
+// BBCodes List of codes result for Parse function
+type BBCodes struct {
+	Original  string
+	NewString string
+	BBCodes   []BBCode
+}
+
+// BBCode struct
+type BBCode struct {
+	OriginalStart int
+	OriginalEnd   int
+	Pos           int
+	Len           int
+	Param         string
+	Name          string
+	IsClose       bool
+	IsValid       bool
+	CloseFor      int
+	OpenFor       int
+}
+
+var validCodes = map[string]bool{
+	"b":     true,
+	"i":     true,
+	"u":     true,
+	"s":     true,
+	"user":  true,
+	"code":  true,
+	"quote": true,
+	"color": true,
+	"size":  true,
+	"url":   true,
+}
+
+// Parse string for valid BBCode and return list of parsed values and newstring
+func Parse(s string) (b BBCodes) {
+	var i, j int
+	var tag BBCode
+	start := false
+	b.Original = s
+	rs := []rune(s)
+	for i = range rs {
+		tag.CloseFor = -1
+		tag.OpenFor = -1
+		if string(rs[i]) == "[" {
+			for j = i + 1; j < len(rs); j++ {
+				if string(rs[j]) == "[" {
+					start = false
+					break
+				}
+				if string(rs[j]) == "]" {
+					start = true
+					tag.Name = ""
+					tag.OriginalStart = i + 1
+					tag.Pos = tag.OriginalStart
+					break
+				}
+			}
+			continue
+		}
+		if string(rs[i]) == "]" && start {
+			start = false
+			tag.OriginalEnd = i + 1
+			tag.Pos = tag.OriginalStart
+			b.BBCodes = append(b.BBCodes, tag)
+			tag = BBCode{}
+			continue
+		}
+
+		if start {
+			tag.Name += string(rs[i])
+		}
+	}
+
+	// Ищем закрыте и выставляем флаг
+	for i = range b.BBCodes {
+		if strings.HasPrefix(b.BBCodes[i].Name, "/") {
+			b.BBCodes[i].IsClose = true
+			b.BBCodes[i].Name = strings.Replace(b.BBCodes[i].Name, "/", "", 1)
+		}
+		if strings.Index(b.BBCodes[i].Name, "=") != -1 {
+			kv := strings.Split(b.BBCodes[i].Name, "=")
+			if len(kv) == 2 {
+				b.BBCodes[i].Name = kv[0]
+				b.BBCodes[i].Param = kv[1]
+			}
+		}
+	}
+
+	c := 0
+	// Проходимся, ищем пары, `c` учитывеат правильный закрывающий тег
+	for i = range b.BBCodes {
+		c = 0
+		if !b.BBCodes[i].IsClose {
+			c++
+			for j = i + 1; j < len(b.BBCodes); j++ {
+				if b.BBCodes[j].IsClose {
+					c--
+				} else {
+					c++
+				}
+				if strings.ToLower(b.BBCodes[i].Name) == strings.ToLower(b.BBCodes[j].Name) &&
+					b.BBCodes[j].IsClose &&
+					(c == 0 || strings.ToLower(b.BBCodes[i].Name) == "code") {
+					b.BBCodes[i].IsValid = validCodes[b.BBCodes[i].Name]
+					b.BBCodes[j].IsValid = validCodes[b.BBCodes[i].Name]
+					b.BBCodes[i].OpenFor = j
+					b.BBCodes[j].CloseFor = i
+					b.BBCodes[i].Len = b.BBCodes[j].OriginalStart - b.BBCodes[i].OriginalEnd - 1
+					break
+				}
+			}
+		}
+	}
+
+	// tag code finder and turn off valid flag
+	inCode := false
+	for i = range b.BBCodes {
+		if strings.ToLower(b.BBCodes[i].Name) == "code" && b.BBCodes[i].IsValid && !inCode {
+			inCode = true
+			continue
+		}
+		if strings.ToLower(b.BBCodes[i].Name) == "code" && b.BBCodes[i].IsClose {
+			inCode = false
+		}
+		if inCode {
+			b.BBCodes[i].IsValid = false
+		}
+	}
+
+	// Make new string, cut untervals and shift positions
+	b.NewString = b.Original
+	for i = 0; i < len(b.BBCodes); i++ {
+		if b.BBCodes[i].IsValid {
+			l := b.BBCodes[i].OriginalEnd - b.BBCodes[i].OriginalStart
+			for j = i + 1; j < len(b.BBCodes); j++ {
+				b.BBCodes[j].Pos -= l + 1
+			}
+			b.NewString = cutString(b.NewString, b.BBCodes[i].Pos, b.BBCodes[i].Pos+l)
+		}
+	}
+
+	for i = 0; i < len(b.BBCodes); i++ {
+		if b.BBCodes[i].IsValid && !b.BBCodes[i].IsClose {
+			p := b.BBCodes[i].OpenFor
+			if p < len(b.BBCodes) && p >= 0 {
+				b.BBCodes[i].Len = b.BBCodes[p].Pos - b.BBCodes[i].Pos
+			}
+		}
+	}
+	return
+}
+
+func cutString(s string, f int, t int) (res string) {
+	for i, r := range []rune(s) {
+		if i+1 < f || i+1 > t {
+			res += string(r)
+		}
+	}
+	return
+}
---
+++ bbcode_test.go
@@ -0,0 +1,74 @@
+package bbcode
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func TestParse(t *testing.T) {
+	table := map[string]string{
+		"[txt=ololo][code][i]II[/i][/zmg]Bo[/b]llo a😪a 🍻 ss": "[txt=ololo][code]II[/zmg]Bo[/b]llo a😪a 🍻 ss",
+		"[code][code][b]sdfs--[/code]":                       "[code][b]sdfs--",
+		"[b][b][b]b[/b]":                                     "[b][b]b",
+		"[b][b][i][b]b[/b]":                                  "[b][b][i]b",
+		"[i]ii[/i][b]b[/b][/b][/b]":                          "iib[/b][/b]",
+		"[i]1[/i][i]z[/i]":                                   "1z",
+		"[color=red]1[color=green]gr[/color]red[/color]":     "1grred",
+		"[[b]][[/b]]":                                        "[][]",
+	}
+	for k, v := range table {
+		s := Parse(k)
+		if s.NewString != v {
+			t.Errorf("TestParse error for : %s\ngot:  %s\nneed: %s\nDebug info: \n%v", k, s.NewString, v, s)
+
+		}
+	}
+}
+
+func BenchmarkParse(b *testing.B) {
+	table := []string{
+		"[txt=ololo][code][i]II[/i][/zmg]Bo[/b]llo a😪a 🍻 ss",
+		"[code][code][b]sdfs--[/code]",
+		"[b][b][b]b[/b]",
+		"[b][b][i][b]b[/b]",
+		"[i]ii[/i][b]b[/b][/b][/b]",
+		"[i]1[/i][i]z[/i]",
+		"[color=red]1[color=green]gr[/color]red[/color]",
+	}
+	rand.Seed(time.Now().UnixNano())
+	j := 0
+	for i := 0; i < b.N; i++ {
+		j++
+		if j > len(table)-1 {
+			j = 0
+		}
+		Parse(table[j])
+	}
+}
+
+func TestPositions(t *testing.T) {
+	s := Parse("[b]b[/b][u]u[/u]")
+	if s.NewString != "bu" {
+		t.Fail()
+	}
+	if len(s.BBCodes) != 4 {
+		t.Fail()
+	}
+	if s.BBCodes[0].Pos != 1 || s.BBCodes[2].Pos != 2 {
+		t.Fail()
+	}
+}
+
+func TestCutString(t *testing.T) {
+	s := "1 2 3 4"
+	if cutString(s, 3, 6) != "1 4" {
+		t.Fail()
+	}
+	if cutString("", 3, 6) != "" {
+		t.Fail()
+	}
+	if cutString("asdasdasdasda", 0, 0) != "asdasdasdasda" {
+		t.Fail()
+	}
+}
